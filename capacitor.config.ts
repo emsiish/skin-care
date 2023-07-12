7bf560bd5974f3839ef52abd2c508fa4434f38c7
Емилия Ушева <emiliyausheva730@gmail.com>
2023-07-12 18:36:09 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'skin-care',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
